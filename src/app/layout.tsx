8e3bc4d74c581c81e521078983ed7b56238bd640
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2026-01-14 09:15:48 +0600

    Impliement route base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/layout/navbar";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 
 
@@ -36,7 +35,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-        <Navbar/>
+       
           {children}
           </ThemeProvider>
       </body>
